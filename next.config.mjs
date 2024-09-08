7cc6059eaa39110493cab201cf0b005934d52c2f
Dulmina Jayaru Geeganage <dulminageeganage59@gmail.com>
2024-09-08 15:10:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-
+  reactStrictMode: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
